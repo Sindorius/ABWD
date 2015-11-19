cbc013e6ae7ffacc7d04ff7ff5c5c58dabca3eca
Nephenee <jkitano@uci.edu>
2015-11-19 15:43:33 -0800

    more ai
    more i say
    more
    better structure and layout for adding more behaviors

--- Classes/Villain.cpp
+++ Classes/Villain.cpp
@@ -1,6 +1,8 @@
 #include "Villain.h"
 #include "Player.h"
 #include <math.h>
+#include <stdlib.h>
+#include "cocos2d.h"
 
 Villain* Villain::create()
 {
@@ -19,41 +21,109 @@ Villain* Villain::create()
 
 void Villain::runAI(std::vector<Player*>* players)
 {
-	std::vector<int> distance;
+	player_list = players;
+	calculations();
+
+	if (behavior_unlocked) {
+		chooseBehavior();
+	}
+
+	switch (behavior) {
+	case 0 :
+		walk();
+		break;
+	case 1 :
+		chargeCharge();
+		break;
+	case 2:
+		charge();
+		break;
+	}
+
+}
+
+void Villain::calculations() {
+	distance.erase(distance.begin(), distance.end());
 	int villainX = this->getPositionX();
 	int villainY = this->getPositionY();
-	for (int i = 0; i < players->size(); i++) {
-		int temp = sqrt(pow((villainX - players->at(i)->getPositionX()), 2) + pow((villainY - players->at(i)->getPositionY()), 2));
+	for (int i = 0; i < player_list->size(); i++) {
+		int temp = sqrt(pow((villainX - player_list->at(i)->getPositionX()), 2) + pow((villainY - player_list->at(i)->getPositionY()), 2));
 		distance.push_back(temp);
 	}
-	switch (behavior) {
-	case(0) :
+}
+
+
+void Villain::chooseBehavior() {
+	int choose = rand() % 2;
+	switch (choose) {
+	case 0:
+		behavior_timer = 150;
+		
+		behavior = 0;
+		break;
+	case 1:
+		behavior_timer = 30;
+		behavior = 1;
+		break;
+	}
+	behavior_unlocked = false;
+}
+
+
+void Villain::walk() {
+	if (behavior_timer > 0) {
+		behavior_timer--;
 		int temp = -1;
 		for (int i = 0; i < distance.size(); i++) {
 			int temp1 = distance[i];
 			if (temp == -1) { temp = temp1; target = i; }
 			else if (temp > temp1) { temp = temp1; target = i; }
-
 		}
 
-		if (this->getPositionX() > players->at(target)->getPositionX())
+
+		if (this->getPositionX() > player_list->at(target)->getPositionX())
 		{
-			this->setPositionX(this->getPositionX() - 1);
+			this->setPositionX(this->getPositionX() - walk_speed);
 		}
-		else if (this->getPositionX() < players->at(target)->getPositionX()) {
-			this->setPositionX(this->getPositionX() + 1);
+		else if (this->getPositionX() < player_list->at(target)->getPositionX()) {
+			this->setPositionX(this->getPositionX() + walk_speed);
 		}
-		if (this->getPositionY() > players->at(target)->getPositionY())
+		if (this->getPositionY() > player_list->at(target)->getPositionY())
 		{
-			this->setPositionY(this->getPositionY() - 1);
+			this->setPositionY(this->getPositionY() - walk_speed);
 		}
-		else if (this->getPositionY() < players->at(target)->getPositionY()) {
-			this->setPositionY(this->getPositionY() + 1);
+		else if (this->getPositionY() < player_list->at(target)->getPositionY()) {
+			this->setPositionY(this->getPositionY() + walk_speed);
 		}
 	}
-
-
+	else {
+		behavior_unlocked = true;
+	}
 }
+
+
+void Villain::charge() {
+	if (behavior_timer > 0) {
+		behavior_timer--;
+		//this->setPositionX(1200);
+		//this->setPositionY(600);
+	}
+	else {
+		behavior_unlocked = true;
+	}
+}
+
+
+void Villain::chargeCharge() {
+	if (behavior_timer > 0) {
+		behavior_timer--;
+	}
+	else {
+		behavior = 2;
+		behavior_timer = 300;
+	}
+}
+
 
 int Villain::getTarget() {
 	return target;
--- Classes/Villain.h
+++ Classes/Villain.h
@@ -13,9 +13,19 @@ public:
 
 	
 	static Villain* create();
-	static Villain* create(int playernum);
+	//static Villain* create(int playernum);
 	//gameloop villain function
 	void runAI(std::vector<Player*>* players);
+	//update villain knowledge
+	void calculations();
+	//determines which behavior the ai should use
+	void chooseBehavior();
+	//walk to target
+	void walk();
+	//charge at target
+	void charge();
+	//charge charge
+	void chargeCharge();
 	//the index of the ai's current target
 	int getTarget();
 	//the current action type the villain uses, use to determine animations and whatnot
@@ -27,11 +37,19 @@ private:
 
 	int target = 0;
 	int behavior = 0;
+	int behavior_timer = 0;
+	int walk_speed = 1;
+	int charge_speed = 4;
+	std::vector<Player*>* player_list;
+	std::vector<int> distance;
+	
+	bool behavior_unlocked = true;
 
 };
 /*
 behavior code
 code 0 - ai will walk towards the specified target parameter with no special actions
+code 1 - ai does a charge sequence
 */
 
 #endif // _VILLAIN_HPP_
